6f75f4e47c199665f4c054c76a6ca6e85a05ad3a
Kensuke Nagae <kyanny@gmail.com>
2013-11-28 14:53:13 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Livechat::Application.config.secret_key_base = 'ca3947fc30e5434398e508f8ce34f13ba21d22323ac53f9f8351bd0e0985e3ec6cc38d1ffd56c8a4ebcfa45639af3e6434a70e6165c0bc33e5de6bf790835ddb'
